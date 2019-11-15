40457bfd7daeb0597af02825f01b023891190473
Enoch Shum <eys2117@columbia.edu>
2019-11-14 21:59:01 -0500

    minor mod

--- Project/settings.py
+++ Project/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
