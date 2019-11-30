01216c48a33fcd860929850e8fd7be0054e266a4
Enoch Shum <eys2117@columbia.edu>
2019-11-30 11:46:52 -0500

    INCOMPLETE_CHUNKED_ENCODING troublehshooting

--- Project/settings.py
+++ Project/settings.py
@@ -46,7 +46,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+]
+
+MIDDLEWARE_CLASSES = [
     'django.middleware.http.ConditionalGetMiddleware',
 ]
 
