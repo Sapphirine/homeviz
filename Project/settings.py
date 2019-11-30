5089660888ddd317d3f35dee4c908867c8ef3ff4
Enoch Shum <eys2117@columbia.edu>
2019-11-30 00:30:08 -0500

    INCOMPLETE_CHUNKED_ENCODING troublehshooting

--- Project/settings.py
+++ Project/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
 ]
 
 ROOT_URLCONF = 'Project.urls'
